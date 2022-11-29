27383a923f72ad05768315489eedb0ae8ebac8b5
AtlantaPatterson <atlantarpatterson@gmail.com>
2022-11-29 13:02:36 -0800

    commit tree 6754dcc7dbed68d599b329716e69834b42bf23bc

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,22 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+    validate :click_bait, on: :create
+
+    def click_bait
+        if TITLE_CLICK_BAIT.none? { |s| s.match title }
+            errors.add :title, "Not enough click bait!"
+        end
+    end
+
+    private
+    TITLE_CLICK_BAIT = [
+        /Won't Believe/i, 
+        /Secret/i, 
+        /Top\d/i, 
+        /Guess/i
+    ]
+
 end
